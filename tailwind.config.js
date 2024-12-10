52371ad43993058e9adc0c65d53165ad2682ae46
Nelson5614 <rmathebeng@gmail.com>
2024-12-10 17:53:44 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#011fa3',
+        'secondary': '#009743',
+      }
+    },
+  },
+  plugins: [],
+}
